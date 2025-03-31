2e836b1d078c17b4b5eb802009c24d300527ecc2
SchneLu <schneiderl.wwi23@student.dhbw-heidenheim.de>
2025-03-31 21:40:18 +0200

    was geändert

--- reset-mongo.sh
+++ reset-mongo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mongoimport --host mongo --db geoDB --collection brunoCollection --type json --file /docker-entrypoint-initdb.d/brunoCollection.json --jsonArray
+
 # Lösche alle Daten im MongoDB-Datenverzeichnis
 rm -rf /data/db/*
 echo "Datenbank zurückgesetzt, Initialisierungsskripte werden ausgeführt..."
